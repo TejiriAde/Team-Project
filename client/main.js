6ec06be69b1f2b3933c9535a3255c95c93af6542
Theo Reeves <theo_25@icloud.com>
2024-06-18 15:29:37 +0100

    appended reviews to the dom and made the form submit to database too as well as clear the form once the user has done this

--- client/main.js
+++ client/main.js
@@ -1,24 +1,52 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.getElementById("game-form");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function fetchAndRenderGameForm() {
+  const response = await fetch("http://localhost:7430/reviews");
+  const userReviews = await response.json();
+  console.log(userReviews);
+  const reviewDiv = document.getElementById("review-info-container");
+  reviewDiv.innerHTML = "";
 
-setupCounter(document.querySelector('#counter'))
+  userReviews.forEach((msg) => {
+    const userReviewsDiv = document.createElement("div");
+    userReviewsDiv.id = "inputReviews";
+    userReviewsDiv.innerHTML = ` Game Name: ${msg.game_name} <br> Rating: ${msg.rating}<br> Username: ${msg.username} <br> Review: ${msg.review} </p>`;
+    reviewDiv.appendChild(userReviewsDiv);
+  });
+  function updateScroll() {
+    let element = document.getElementById("review-info-container");
+    element.scrollTop = element.scrollHeight;
+  }
+  updateScroll();
+}
+fetchAndRenderGameForm();
+
+form.addEventListener("submit", submitButton);
+async function submitButton(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+
+  try {
+    const response = await fetch("http://localhost:7430/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formValues),
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      console.log("DATA IS SAVED - ALL IS WELL");
+      fetchAndRenderGameForm();
+      form.reset();
+      // console.log(formValues);
+    } else {
+      console.log("NOOO IT DIDN'T WORK!!!!!");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
